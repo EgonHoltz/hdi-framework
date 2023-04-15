184dd8576b90852ab87ee6706546780f51d17bf1
Egon Holtz <egonholtz@gmail.com>
2023-04-15 11:20:48 +0100

    Added connection with database to validate the msg

--- GrpcServiceApplication/src/main/java/pt/hdi/grpcservice/controller/MessageController.java
+++ GrpcServiceApplication/src/main/java/pt/hdi/grpcservice/controller/MessageController.java
@@ -1,4 +1,6 @@
-package pt.hdi.grpcservice.service;
+package pt.hdi.grpcservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
@@ -6,10 +8,15 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import pt.hdi.grpcservice.ServiceDataRequest;
 import pt.hdi.grpcservice.ServiceDataResponse;
 import pt.hdi.grpcservice.dataGrpc.dataImplBase;
+import pt.hdi.grpcservice.model.Configuration;
+import pt.hdi.grpcservice.service.ConfigurationService;
 
 @GrpcService
-public class MessageService extends dataImplBase {
+public class MessageController extends dataImplBase {
 
+	@Autowired
+	private ConfigurationService confService;
+	
 	@Override
 	public void sendRqst(ServiceDataRequest request, StreamObserver<ServiceDataResponse> responseObserver) {
 		String clientId = request.getClientId();
@@ -17,6 +24,12 @@ public class MessageService extends dataImplBase {
 		
 		if (clientId.isEmpty() || msg.isEmpty()) {
 			responseObserver.onError(Status.FAILED_PRECONDITION.withDescription("clientID or msg is empty").asRuntimeException());
+		}
+		
+		Configuration clientDoc = confService.getByDocumentName(clientId);
+		
+		if(clientDoc == null) {
+			responseObserver.onError(Status.FAILED_PRECONDITION.withDescription("client not found").asRuntimeException());			
 		}
 		
 		ServiceDataResponse response = ServiceDataResponse.newBuilder().setClientId(clientId).setJsonMsg("OK").build();
---
+++ GrpcServiceApplication/src/main/java/pt/hdi/grpcservice/service/ConfigurationService.java
@@ -0,0 +1,42 @@
+package pt.hdi.grpcservice.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pt.hdi.grpcservice.model.Configuration;
+import pt.hdi.grpcservice.repository.ConfigurationRepository;
+
+
+@Service
+public class ConfigurationService {
+	
+	@Autowired
+	private ConfigurationRepository confRep;
+	
+	public List<Configuration> getAllConfigs(){
+		return confRep.findAll();
+	}
+	
+	public Configuration getByDocumentName(String docName) {
+		return confRep.findByDocumentName(docName);
+	}
+	
+	public boolean createNewConfiguration(Configuration config) {
+		if (getByDocumentName(config.getDocumentName()) == null) {
+			confRep.insert(config);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean saveConfiguration(Configuration conf) {
+		Configuration savedConf = confRep.save(conf);
+		if (savedConf != null) {
+			return true;
+		}
+		return false;
+	}
+	
+}
